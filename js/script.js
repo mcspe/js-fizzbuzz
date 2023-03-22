aba846b3474699fca6dd0825fadfc7403fde88cb
mcspe <mcspezzaferro@gmail.com>
2023-03-22 14:14:21 +0100

    [add] basic display

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-for(let i=1; i<=20; i++){
-  //console.log(i);
+const container = document.querySelector('.container');
+
+for(let i=1; i<=100; i++){
+
+  const box = document.createElement('div');
+  box.classList.add('box');
+  container.append(box);
+
+  let text = '';
+  
   if ((i % 3) && (i % 5)){
-    console.log(i);
+    text = i;
+    // box.append(text);
   } else{
-    if(!(i % 3)) console.log('fizz');
-    if(!(i % 5)) console.log('buzz');
+    if(!(i % 3)){
+      text += 'Fizz';
+    } 
+    if(!(i % 5)){
+      text += 'Buzz';
+    } 
   }
+
+  box.append(text);
+
 }
